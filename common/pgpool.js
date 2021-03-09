eae001b45d9e7ac8ba45e148613c8d874813c427
lightfuel <easymovet@gmail.com>
2021-03-09 14:02:58 -0600

    ssl

--- common/pgpool.js
+++ common/pgpool.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 const pgpool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
- 
- 
+
+
 module.exports = pgpool;
