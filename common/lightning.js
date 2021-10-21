0ab90cc9dde0eeb47548998da0b16a0a36b0d1d8
missaghi <easymovet@gmail.com>
2021-10-21 13:04:53 -0500

    trycatch

--- common/lightning.js
+++ common/lightning.js
@@ -1,17 +1,20 @@
-var node = "";
+var LndGrpc = require('lnd-grpc');
+var node = new LndGrpc({
+    host: process.env.LNDURL,
+    macaroon: process.env.MACAROON,
+    waitForCert: true,
+});
 var light = async() => {
     console.info("lightning RPC accessed", node)
     if (node == "") {
-        console.info("lightning RPC connecting" )
-        var LndGrpc = require('lnd-grpc');
+        console.info("lightning RPC connecting" ) 
 
-        node = new LndGrpc({
-            host: process.env.LNDURL,
-            macaroon: process.env.MACAROON
-        })
+        
+try {
+        await node.connect(); console.info("lightning RPC created", node.services)}
+        catch (e) { console.log(e);}
 
-        await node.connect();
-        console.info("lightning RPC created", node.services)
+        
 
     }
 
