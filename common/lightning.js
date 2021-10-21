59afdc545245a580b7274293c8f12b6d6106ab50
missaghi <easymovet@gmail.com>
2021-10-21 13:15:59 -0500

    unlock

--- common/lightning.js
+++ common/lightning.js
@@ -1,24 +1,39 @@
 var LndGrpc = require('lnd-grpc');
-var node = new LndGrpc({
+var grpc = new LndGrpc({
     host: process.env.LNDURL,
     macaroon: process.env.MACAROON,
     waitForCert: true,
 });
 var light = async() => {
-    console.info("lightning RPC accessed", node.state)
-    if (node.state == "disconnected") {
-        console.info("lightning RPC connecting" ) 
+    console.info("lightning RPC accessed", grpc.state)
+    if (grpc.state == "disconnected") {
+        console.info("lightning RPC connecting" )  
 
+        await grpc.connect()
+
+        // Do something cool if we detect that the wallet is locked.
+        grpc.on(`locked`, () => console.log('wallet locked!'))
+
+        // Do something cool when the wallet gets unlocked.
+        grpc.on(`active`, () => console.log('wallet unlocked!'))
+
+        // Do something cool when the connection gets disconnected.
+        grpc.on(`disconnected`, () => console.log('disconnected from lnd!'))
+
+        // Check if the wallet is locked and unlock if needed.
+        if (grpc.state === 'locked') {
+        const { WalletUnlocker } = grpc.services
+        await WalletUnlocker.unlockWallet({
+            wallet_password: Buffer.from('mypassword'),
+        })
+        // After unlocking the wallet, activate the Lightning service and all of it's subservices.
+        await WalletUnlocker.activateLightning()
         
-try {
-        await node.connect(); console.info("lightning RPC created", node.services)}
-        catch (e) { console.log(e);}
-
-        
-
+        }
+ 
     }
 
-    return node.services.Lightning;
+    return grpc.services.Lightning;
 
 }
 
